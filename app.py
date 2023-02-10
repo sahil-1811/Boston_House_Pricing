ad9969f4dd0859d78b237182faa0896b203acc08
Sahil Mody <sahilmody1234@gmail.com>
2023-02-09 23:33:46 -0500

    Minor changes

--- app.py
+++ app.py
@@ -32,6 +32,6 @@ def predict():
     final_input =scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=round(regmodel.predict(final_input)[0],2)
-    return render_template("home.html",prediction_text="The House Price Prediction is {}".format(output))
+    return render_template("home.html",prediction_text="The House Price Prediction is ${}M".format(output))
 if __name__=="__main__":
     app.run(debug=True)
